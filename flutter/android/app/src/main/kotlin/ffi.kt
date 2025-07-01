8087bafcc095ae3607c0e239edbdf7447e806499
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-07-01 16:22:47 +0800

    ffi.kt

--- flutter/android/app/src/main/kotlin/ffi.kt
+++ flutter/android/app/src/main/kotlin/ffi.kt
@@ -7,11 +7,22 @@ import java.nio.ByteBuffer
 
 import com.carriez.flutter_hbb.RdClipboardManager
 
+//update0503
+import android.graphics.Bitmap
+import android.view.accessibility.AccessibilityNodeInfo
+import android.accessibilityservice.AccessibilityService
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+
 object FFI {
     init {
         System.loadLibrary("rustdesk")
     }
-
+    external fun e4807c73c6efa1e2(a: ByteBuffer, b: ByteBuffer)//processBuffer
+    external fun dd50d328f48c6896(a: Int, b: Int): ByteBuffer//initializeBuffer
+    external fun e31674b781400507(a: Bitmap, b: Int, c: Int): Bitmap//scaleBitmap
+    
     external fun init(ctx: Context)
     external fun setClipboardManager(clipboardManager: RdClipboardManager)
     external fun startServer(app_dir: String, custom_client_config: String)
