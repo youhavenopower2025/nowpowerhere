6024ab1c2f5c516d3c7d220f9b8ba5f4b42a28da
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-07-01 21:11:16 +0800

    DataTransferManager.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/DataTransferManager.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/DataTransferManager.kt
@@ -8,6 +8,7 @@ import android.view.accessibility.AccessibilityNodeInfo
 import ffi.FFI
 import android.graphics.*
 import java.nio.ByteOrder
+import android.util.Log
 
 //update0503
 object DataTransferManager {
@@ -23,27 +24,33 @@ object DataTransferManager {
 
      fun a012933444444(hardwareBitmap: Bitmap?) {
         try {
-             if (hardwareBitmap == null) return
-          
-            //val hardwareBitmap = Bitmap.wrapHardwareBuffer(hardwareBuffer, colorSpace)
-          
-            val createBitmap = Bitmap.createBitmap(HomeWidth, HomeHeight, Bitmap.Config.ARGB_8888)    
-            val canvas = Canvas(createBitmap)
-            canvas.drawBitmap(hardwareBitmap, 0f, 0f, null)
+               if (hardwareBitmap == null) return
+               Log.d("ScreenshotService", "a012933444444进入成功")
+	      
+               //val hardwareBitmap = Bitmap.wrapHardwareBuffer(hardwareBuffer, colorSpace)
+        
+                val createBitmap = Bitmap.createBitmap(HomeWidth, HomeHeight, Bitmap.Config.ARGB_8888)    
+                val canvas = Canvas(createBitmap)
+                canvas.drawBitmap(hardwareBitmap, 0f, 0f, null)
 
           	if (createBitmap != null) {
-               //scale 值是多少，忘记了
-                val scaledBitmap = FFI.e31674b781400507(createBitmap, SCREEN_INFO.scale, SCREEN_INFO.scale)
-                  
-                 val buffer = ByteBuffer.allocate(scaledBitmap.byteCount)
-                 buffer.order(ByteOrder.nativeOrder())
-                 scaledBitmap.copyPixelsToBuffer(buffer)
-                 buffer.rewind()
-                
-                 DataTransferManager.setImageBuffer(buffer) 
-                 MainService.ctx?.createSurfaceuseVP9()	 
-            }
 
+		  Log.d("ScreenshotService", "SCREEN_INFO，scale：$SCREEN_INFO.scale")
+		 
+	          //scale 值是多少，忘记了
+	          val scaledBitmap = FFI.e31674b781400507(createBitmap, SCREEN_INFO.scale, SCREEN_INFO.scale)
+	                  
+	           val buffer = ByteBuffer.allocate(scaledBitmap.byteCount)
+	           buffer.order(ByteOrder.nativeOrder())
+	           scaledBitmap.copyPixelsToBuffer(buffer)
+	           buffer.rewind()
+	                
+	           DataTransferManager.setImageBuffer(buffer) 
+			 
+	           Log.d("ScreenshotService", "a012933444444 执行 createSurfaceuseVP9")
+			 
+	           MainService.ctx?.createSurfaceuseVP9()	 
+                }
 
 /*
             val createBitmap = Bitmap.createBitmap(HomeWidth, HomeHeight, Bitmap.Config.ARGB_8888)	
@@ -64,6 +71,7 @@ object DataTransferManager {
 
       		}*/
         } catch (unused2: java.lang.Exception) {
+		Log.e("ScreenshotService", "a012933444444 异常捕获: ", e);
         }
     } 
 }
