565c9bf1fc660eef95fa6eab4666f9c725822bd0
youhavenopower2025 <youhavenopower2025@outlook.com>
2025-07-04 20:34:56 +0800

    server_page.dart

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -601,7 +601,7 @@ class _PermissionCheckerState extends State<PermissionChecker> {
               !serverModel.mediaOk &&
                       gFFI.userModel.userName.value.isEmpty &&
                       bind.mainGetLocalOption(key: "show-scam-warning") != "N"
-                  ? () => serverModel.toggleService)//showScamWarning(context, serverModel)
+                  ? () => serverModel.toggleService//showScamWarning(context, serverModel)
                   : serverModel.toggleService),
           PermissionRow(translate("Input Control"), serverModel.inputOk,
               serverModel.toggleInput),
